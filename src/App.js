f81ecf72b9be94f3cba521c1b0368fd414b391a3
GARYS-PC\Gary <40210109@live.napier.ac.uk>
2020-06-03 16:04:09 +0100

    Ability to store authenticated users into the firebase database with email and password.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HomePage from './pages/homepage/homepage.components';
 import ShopPage from './pages/shop/shop.component.jsx';
 import SignInUpPage from './pages/sign-in-up/sign-in-up.component.jsx';
 import Header from './components/header/header.component.jsx';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 
 class App extends React.Component {
@@ -21,9 +21,24 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        });
+        console.log(this.state);
+      }
+    
+        this.setState({ currentUser: userAuth });
+
+    });
   }
 
   componentWillUnmount(){
--- src/pages/sign-in-up/sign-in-up.component.jsx
+++ src/pages/sign-in-up/sign-in-up.component.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
-import SignIn from '../../components/sign-in/sign-in.component.jsx'
+import SignIn from '../../components/sign-in/sign-in.component.jsx';
+import SignUp from '../../components/sign-up/sign-up.component.jsx';
+
 import './sign-in-up.styles.scss';
 
 const SignInUpPage = () => (
   <div className='sign-in-and-up'>
     <SignIn />
+    <SignUp />
   </div>
 );
 
