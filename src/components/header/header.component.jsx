cda46834b23ff5ebaec6ccbaf637561a1e603b36
GARYS-PC\Gary <garymurie@hotmail.com>
2020-06-17 20:14:47 +0100

    Started cart, styles and compoentns in place

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
@@ -25,8 +28,9 @@ const Header = ({ currentUser }) => (
         :
         (<Link className='option' to='signin'>SIGN IN</Link>)
       }
+      <CartIcon />
     </div>
-
+    <CartDropdown />
   </div>
 );
 
